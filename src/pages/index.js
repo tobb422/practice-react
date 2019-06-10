c00ecf0f1565e802263ee999ed383e89deb125fb
Toshiki Baba <yz.t0.r33@gmail.com>
2019-06-11 01:47:58 +0900

    一覧表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,36 @@ import '../styles/pages/index.css';
 import axios from 'axios';
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: [],
+    }
+  }
+
   async componentDidMount() {
     const tmp = await axios.get('https://www.googleapis.com/books/v1/volumes?q=React')
-    console.log(tmp)
+    this.setState({ books: tmp.data.items })
   }
+
   render() {
+    const books = this.state.books
     return (
-      <h1>テスト</h1>
+      <main>
+        <h1>本検索</h1>
+        <div className="wrapper">
+          { books.map(book => {
+            const data = book.volumeInfo
+            return (
+              <div key={book.id} className="book">
+                <h3>{data.title}</h3>
+                <p>{data.description}</p>
+                <img src={data.imageLinks.thumbnail} />
+              </div>
+            )
+          })}
+        </div>
+      </main>
     )
   }
 }
