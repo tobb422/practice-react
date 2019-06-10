0e705e0a77df47c1bdc13b490ebdd3fd612bf799
Toshiki Baba <yz.t0.r33@gmail.com>
2019-06-11 01:29:31 +0900

    axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import '../styles/pages/index.css';
+import axios from 'axios';
 
 export default class Index extends Component {
+  async componentDidMount() {
+    const tmp = await axios.get('https://www.googleapis.com/books/v1/volumes?q=React')
+    console.log(tmp)
+  }
   render() {
-    return <h1>テスト</h1>
+    return (
+      <h1>テスト</h1>
+    )
   }
 }
